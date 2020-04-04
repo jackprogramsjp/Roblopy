d9740f200589626062b80e0e4bf936736f062112
Jack Programs <52725440+jackprogramsjp@users.noreply.github.com>
2020-04-04 16:45:00 -0700

    Version 3.0

---
+++ roblopy/assets.py
@@ -0,0 +1,54 @@
+from roblopy.utils.request import get
+
+class Asset:
+    ProductType = None
+    AssetId = None
+    ProductId = None
+    Name = None
+    Description = None
+    AssetTypeId = None
+    Creator = None
+    CreatorId = None
+    CreatorType = None
+    Created = None
+    Updated = None
+    PriceInRobux = None
+    Sales = None
+    IsNew = None
+    IsForSale = None
+    IsPublicDomain = None
+    IsLimited = None
+    IsLimitedUnique = None
+
+    def __init__(self, assetId):
+        response = get("https://api.roblox.com/Marketplace/ProductInfo?assetId=" + str(assetId)).json()
+
+        self.ProductType = response["ProductType"]
+        self.AssetId = response["AssetId"]
+        self.ProductId = response["ProductId"]
+        self.Name = response["Name"]
+        self.Description = response["Description"]
+        self.AssetTypeId = response["AssetTypeId"]
+        self.Creator = response["Creator"]["Name"]
+        self.CreatorId = response["Creator"]["Id"]
+        self.CreatorType = response["Creator"]["CreatorType"]
+        self.Created = response["Created"]
+        self.Updated = response["Updated"]
+        self.PriceInRobux = response["PriceInRobux"]
+        self.Sales = response["Sales"]
+        self.IsNew = response["IsNew"]
+        self.IsForSale = response["IsForSale"]
+        self.IsPublicDomain = response["IsPublicDomain"]
+        self.IsLimited = response["IsLimited"]
+        self.IsLimitedUnique = response["IsLimitedUnique"]
+
+class Assets:
+    @staticmethod
+    def GetAsset(assetId):
+        return get("https://api.roblox.com/Marketplace/ProductInfo?assetId=" + str(assetId)).json()
+
+    @staticmethod
+    def UserHasAsset(userId, assetId):
+        return get(
+            "https://api.roblox.com/Ownership/HasAsset?userId=" + str(userId) + "&assetId=" + str(assetId)).json()
+
---
+++ roblopy/groups.py
@@ -0,0 +1,92 @@
+from roblopy.utils.request import get
+
+class Group:
+    Name = None
+    Id = None
+    Owner = None
+    OwnerName = None
+    OwnerId = None
+    EmblemUrl = None
+    Description = None
+    Roles = None
+
+    def __init__(self, groupId):
+        response = get("https://api.roblox.com/groups/" + str(groupId)).json()
+
+        self.Name = response["Name"]
+        self.Id = response["Id"]
+        self.Owner = response["Owner"]
+        self.OwnerName = response["Owner"]["Name"]
+        self.OwnerId = response["Owner"]["Id"]
+        self.EmblemUrl = response["EmblemUrl"]
+        self.Description = response["Description"]
+        self.Roles = response["Roles"]
+
+class Groups:
+    @staticmethod
+    def GetUsersGroups(userId):
+        response = get("https://api.roblox.com/users/" + str(userId) + "/groups").json()
+
+        return [group["Name"] for group in response]
+
+    @staticmethod
+    def GetUsersGroupIds(userId):
+        response = get("https://api.roblox.com/users/" + str(userId) + "/groups").json()
+
+        return [group["Id"] for group in response]
+
+    @staticmethod
+    def GetGroup(groupId):
+        return get("https://api.roblox.com/groups/" + str(groupId)).json()
+
+    @staticmethod
+    def GetGroupName(groupId):
+        return get("https://api.roblox.com/groups/" + str(groupId)).json()["Name"]
+
+    @staticmethod
+    def GetGroupOwner(groupId):
+        return get("https://api.roblox.com/groups/" + str(groupId)).json()["Owner"]["Name"]
+
+    @staticmethod
+    def GetGroupDescription(groupId):
+        return get("https://api.roblox.com/groups/" + str(groupId)).json()["Description"]
+
+    @staticmethod
+    def GetGroupRoles(groupId):
+        return get("https://api.roblox.com/groups/" + str(groupId)).json()["Roles"]
+
+    @staticmethod
+    def GetGroupRoleNames(groupId):
+        response = get("https://api.roblox.com/groups/" + str(groupId)).json()["Roles"]
+
+        return [role["Name"] for role in response]
+
+    @staticmethod
+    def GetGroupRoleNumbers(groupId):
+        response = get("https://api.roblox.com/groups/" + str(groupId)).json()["Roles"]
+
+        return [role["Rank"] for role in response]
+
+    @staticmethod
+    def GetGroupAllies(groupId):
+        return get("https://api.roblox.com/groups/" + str(groupId) + "/allies").json()
+
+    @staticmethod
+    def GetGroupAllyNames(groupId):
+        return get("https://api.roblox.com/groups/" + str(groupId) + "/allies").json()["Groups"]
+
+    @staticmethod
+    def GetRecentAllyOfGroup(groupId):
+        return get("https://api.roblox.com/groups/" + str(groupId) + "/allies").json()["Groups"][0]
+
+    @staticmethod
+    def GetGroupEnemies(groupId):
+        return get("https://api.roblox.com/groups/" + str(groupId) + "/enemies").json()
+
+    @staticmethod
+    def GetGroupEnemyNames(groupId):
+        return get("https://api.roblox.com/groups/" + str(groupId) + "/enemies").json()["Groups"]
+
+    @staticmethod
+    def GetRecentEnemyOfGroup(groupId):
+        return get("https://api.roblox.com/groups/" + str(groupId) + "/enemies").json()["Groups"][0]
