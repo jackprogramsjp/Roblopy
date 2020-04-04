c8e5b96874b673186ec64321758c09e182abf9a2
Jack Programs <52725440+jackprogramsjp@users.noreply.github.com>
2020-04-04 16:43:09 -0700

    Delete __init__.py

--- roblopy/__init__.py
+++
@@ -1,9 +0,0 @@
-# Classes
-from roblopy.user import Users
-from roblopy.user import GetUserFromUsername
-from roblopy.user import GetUserFromId
-from roblopy.friends import Friends
-from roblopy.assets import Assets
-from roblopy.assets import Asset
-from roblopy.groups import Groups
-from roblopy.groups import Group
