f62180b605c5191f24b567add207ff026a432c73
Jack Programs <52725440+jackprogramsjp@users.noreply.github.com>
2020-04-04 16:44:11 -0700

    Added more functions

--- roblopy/user.py
+++ roblopy/user.py
@@ -1,47 +1,33 @@
-import requests
+from roblopy.utils.request import get
 
-class GetUserFromUsername:
+class User:
     Id = None
     Username = None
-    AvatarUri = None
-    AvatarFinal = None
-    IsOnline = None
-
-    def __init__(self, username):
-        response = requests.get("http://api.roblox.com/users/get-by-username?username=" + str(username)).json()
-
-        self.Id = response["Id"]
-        self.Username = response["Username"]
-        self.AvatarUri = response["AvatarUri"]
-        self.AvatarFinal = response["AvatarFinal"]
-        self.IsOnline = response["IsOnline"]
-
-class GetUserFromId:
-    Id = None
-    Username = None
-    AvatarUri = None
-    AvatarFinal = None
-    IsOnline = None
 
     def __init__(self, userId):
-        response = requests.get("http://api.roblox.com/users/" + str(userId)).json()
+        response = get("http://api.roblox.com/users/" + str(userId)).json()
 
         self.Id = response["Id"]
         self.Username = response["Username"]
-        self.AvatarUri = response["AvatarUri"]
-        self.AvatarFinal = response["AvatarFinal"]
-        self.IsOnline = response["IsOnline"]
 
 class Users:
     @staticmethod
     def GetUsernameFromId(userId):
-        return requests.get("http://api.roblox.com/users/" + str(userId)).json()["Username"]
+        return get("http://api.roblox.com/users/" + str(userId)).json()["Username"]
 
     @staticmethod
     def GetIdFromUsername(username):
-        return requests.get("http://api.roblox.com/users/get-by-username?username=" + str(username)).json()["Id"]
+        return get("http://api.roblox.com/users/get-by-username?username=" + str(username)).json()["Id"]
+
+    @staticmethod
+    def IsOnline(userId):
+        return get(f"https://api.roblox.com/users/{userId}/onlinestatus/").json()["IsOnline"]
+
+    @staticmethod
+    def GetOnlineStatus(userId):
+        return get(f"https://api.roblox.com/users/{userId}/onlinestatus/").json()
 
     @staticmethod
     def CanManageAsset(userId, assetId):
-        return requests.get("http://api.roblox.com/users/" + str(userId) + "/canmanage/" + str(assetId)).json() \
+        return get("http://api.roblox.com/users/" + str(userId) + "/canmanage/" + str(assetId)).json() \
             ["CanManage"]
