7ffb67f8d8b4fd97e2a727b406b8cca2fd6fd931
Jack Programs <52725440+jackprogramsjp@users.noreply.github.com>
2020-04-05 10:50:59 -0700

    Version 3.5

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ long_description = open("README.md", "r").read()
 
 setup(
     name="roblopy",
-    packages=["roblopy"],
-    version="3.0",
+    packages=["roblopy", "roblopy.utils"],
+    version="3.5",
     license="MIT",
     description="Roblox API built in Python",
     long_description=long_description,
