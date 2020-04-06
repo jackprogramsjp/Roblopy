c97e0c3724a1e2df370de94334613b291d5c7c56
Jack Programs <52725440+jackprogramsjp@users.noreply.github.com>
2020-04-05 18:50:31 -0700

    Version 3.6

--- roblopy/utils/request.py
+++ roblopy/utils/request.py
@@ -15,3 +15,28 @@ def get(url: str):
         raise BadRequest(f"Got status code {response.status_code} from {url}. Data is {response.text}.")
     else:
         raise BadStatus(f"Got status code {response.status_code} from {url}. Data is {response.text}.")
+
+def no_json_get(url: str):
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        return response
+    elif response.status_code == 404:
+        raise NotFound(f"Got status code {response.status_code} from {url}. Data is {response.text}.")
+    elif response.status_code == 400:
+        raise BadRequest(f"Got status code {response.status_code} from {url}. Data is {response.text}.")
+    else:
+        raise BadStatus(f"Got status code {response.status_code} from {url}. Data is {response.text}.")
+
+def no_data_get(url: str):
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        return response
+    elif response.status_code == 404:
+        raise NotFound(f"Got status code {response.status_code} from {url}.")
+    elif response.status_code == 400:
+        raise BadRequest(f"Got status code {response.status_code} from {url}.")
+    else:
+        raise BadStatus(f"Got status code {response.status_code} from {url}.")
+                           
