713f80a8ef0784ddc3ad610af67628649867cc25
Jack Programs <52725440+jackprogramsjp@users.noreply.github.com>
2020-04-05 18:40:11 -0700

    Version 3.6

--- roblopy/user.py
+++ roblopy/user.py
@@ -20,6 +20,59 @@ class Users:
         return get("http://api.roblox.com/users/get-by-username?username=" + str(username)).json()["Id"]
 
     @staticmethod
+    def IsInGroup(userId, groupId):
+        response = get(f"https://api.roblox.com/users/{userId}/groups").json()
+        in_group = False
+
+        for group in response:
+            if group["Id"] == groupId:
+                in_group = True
+                break
+
+        return in_group
+
+    @staticmethod
+    def GetRankInGroup(userId, groupId):
+        response = get(f"https://api.roblox.com/users/{userId}/groups").json()
+        rank = None
+
+        for group in response:
+            if group["Id"] == groupId:
+                rank = group["Rank"]
+
+        return rank
+
+    @staticmethod
+    def GetRoleInGroup(userId, groupId):
+        response = get(f"https://api.roblox.com/users/{userId}/groups").json()
+        role = None
+
+        for group in response:
+            if group["Id"] == groupId:
+                role = group["Role"]
+
+        return role
+
+    @staticmethod
+    def GroupIsPrimary(userId, groupId):
+        response = get(f"https://api.roblox.com/users/{userId}/groups").json()
+        primary = False
+
+        for group in response:
+            if group["Id"] == groupId and group["IsPrimary"]:
+                primary = True
+
+        return primary
+
+    @staticmethod
+    def GetProfileDescription(userId):
+        response = no_data_get(f"https://www.roblox.com/users/{userId}/profile").content
+        soup = BeautifulSoup(response, "html.parser")
+        description = soup.find("span", {"class": "profile-about-content-text linkify"}, text=True).get_text()
+
+        return description
+
+    @staticmethod
     def IsOnline(userId):
         return get(f"https://api.roblox.com/users/{userId}/onlinestatus/").json()["IsOnline"]
 
