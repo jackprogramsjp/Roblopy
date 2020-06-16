4460c59dc2972704fc0deb396bb066cebbf5a850
Jack Programs <52725440+jackprogramsjp@users.noreply.github.com>
2020-06-15 19:33:17 -0500

    Version 5.0

--- roblopy/user.py
+++ roblopy/user.py
@@ -1,79 +1,128 @@
 from bs4 import BeautifulSoup
 from .utils.request import get, no_data_get
+from typing import Optional
+
 
 class User:
-    def __init__(self, userId):
-        response = get(f"https://users.roblox.com/v1/users/{userId}").json()
-        status = get(f"https://users.roblox.com/v1/users/{userId}/status").json()["status"]
+    """
+    Represents a Roblox user.
+    """
+    def __init__(self, user_id: int):
+        """
+        Construct a new user class.
+        :param user_id: The User's ID.
+        """
+        response = get(f"https://users.roblox.com/v1/users/{user_id}").json()
+        status = get(f"https://users.roblox.com/v1/users/{user_id}/status").json()["status"]
 
-        self.Name = response["name"]
-        self.DisplayName = response["displayName"]
-        self.Id = response["id"]
-        self.IsBanned = response["isBanned"]
-        self.Created = response["created"]
-        self.Description = response["description"] if response["description"] else None
-        self.Status = status if status else None
+        self.name: str = response["name"]
+        self.display_name: str = response["displayName"]
+        self.id: int = response["id"]
+        self.is_banned: bool = response["isBanned"]
+        self.created: str = response["created"]
+        self.description: str = response["description"] if response["description"] else None
+        self.status: str = status if status else None
+
 
 class Users:
     @staticmethod
-    def GetUsernameFromId(userId):
-        return get("http://api.roblox.com/users/" + str(userId)).json()["Username"]
+    def get_username_from_id(user_id: int) -> str:
+        """
+        Gets the User's name from their User ID.
+        :param user_id: The User ID.
+        :return: The User's name.
+        """
+        return get("http://api.roblox.com/users/" + str(user_id)).json()["Username"]
 
     @staticmethod
-    def GetIdFromUsername(username):
+    def get_id_from_username(username: str) -> int:
+        """
+        Gets the User's ID from their username.
+        :param username: The User's name.
+        :return: The User's ID.
+        """
         return get("http://api.roblox.com/users/get-by-username?username=" + str(username)).json()["Id"]
 
     @staticmethod
-    def IsInGroup(userId, groupId):
-        response = get(f"https://api.roblox.com/users/{userId}/groups").json()
+    def is_in_group(user_id: int, group_id: int) -> bool:
+        """
+        Checks if the User is in a specific group.
+        :param user_id: The User's ID to check for.
+        :param group_id: The Group's ID to check for.
+        :return: True or False.
+        """
+        response = get(f"https://api.roblox.com/users/{user_id}/groups").json()
         in_group = False
 
         for group in response:
-            if group["Id"] == groupId:
+            if group["Id"] == group_id:
                 in_group = True
                 break
 
         return in_group
 
     @staticmethod
-    def GetRankInGroup(userId, groupId):
-        response = get(f"https://api.roblox.com/users/{userId}/groups").json()
+    def get_rank_in_group(user_id: int, group_id: int) -> int:
+        """
+        Gets the User's rank of the specific group.
+        :param user_id: The User's ID.
+        :param group_id: The Group's ID.
+        :return: The rank of the User in the group.
+        """
+        response = get(f"https://api.roblox.com/users/{user_id}/groups").json()
         rank = None
 
         for group in response:
-            if group["Id"] == groupId:
+            if group["Id"] == group_id:
                 rank = group["Rank"]
                 break
 
         return rank
 
     @staticmethod
-    def GetRoleInGroup(userId, groupId):
-        response = get(f"https://api.roblox.com/users/{userId}/groups").json()
+    def get_role_in_group(user_id: int, group_id: int) -> str:
+        """
+        Gets the User's role of the specific group.
+        :param user_id: The User's ID.
+        :param group_id: The Group's ID.
+        :return: The role of the User in the group.
+        """
+        response = get(f"https://api.roblox.com/users/{user_id}/groups").json()
         role = None
 
         for group in response:
-            if group["Id"] == groupId:
+            if group["Id"] == group_id:
                 role = group["Role"]
                 break
 
         return role
 
     @staticmethod
-    def GroupIsPrimary(userId, groupId):
-        response = get(f"https://api.roblox.com/users/{userId}/groups").json()
+    def group_is_primary(user_id: int, group_id: int) -> bool:
+        """
+        Checks if User's specific group is primary.
+        :param user_id: The User's ID to check for.
+        :param group_id: The Group's ID to check for.
+        :return: True or False.
+        """
+        response = get(f"https://api.roblox.com/users/{user_id}/groups").json()
         primary = False
 
         for group in response:
-            if group["Id"] == groupId and group["IsPrimary"]:
+            if group["Id"] == group_id and group["IsPrimary"]:
                 primary = True
                 break
 
         return primary
 
     @staticmethod
-    def GetProfileDescription(userId):
-        response = get(f"https://users.roblox.com/v1/users/{userId}").json()["description"]
+    def get_profile_description(user_id: int) -> Optional[str]:
+        """
+        Gets the User's bio / description.
+        :param user_id: The User's ID.
+        :return: The User's bio / description, but will return None if no bio / description.
+        """
+        response = get(f"https://users.roblox.com/v1/users/{user_id}").json()["description"]
 
         if response == "":
             return None
@@ -81,8 +130,13 @@ class Users:
         return response
 
     @staticmethod
-    def GetProfileStatus(userId):
-        response = get(f"https://users.roblox.com/v1/users/{userId}/status").json()["status"]
+    def get_profile_status(user_id: int) -> Optional[str]:
+        """
+        Gets the User's status.
+        :param user_id: The User's ID.
+        :return: The User's status, but will return None if no status.
+        """
+        response = get(f"https://users.roblox.com/v1/users/{user_id}/status").json()["status"]
 
         if response == "":
             return None
@@ -113,8 +167,13 @@ class Users:
     #     return status
 
     @staticmethod
-    def GetAvatarImage(userId):
-        response = no_data_get(f"https://www.roblox.com/users/{userId}/profile").content
+    def get_avatar_image(user_id: int) -> Optional[str]:
+        """
+        Gets the User's avatar image.
+        :param user_id: The User's ID.
+        :return: The User's avatar image, but will return None if avatar image cannot be found or it doesn't exist.
+        """
+        response = no_data_get(f"https://www.roblox.com/users/{user_id}/profile").content
         soup = BeautifulSoup(response, "html.parser")
 
         try:
@@ -125,18 +184,39 @@ class Users:
             return image
 
     @staticmethod
-    def IsBanned(userId):
-        return get(f"https://users.roblox.com/v1/users/{userId}").json()["isBanned"]
+    def is_banned(user_id: int) -> bool:
+        """
+        Checks if User is banned.
+        :param user_id: The User's ID to check for.
+        :return: True or False.
+        """
+        return get(f"https://users.roblox.com/v1/users/{user_id}").json()["isBanned"]
 
     @staticmethod
-    def IsOnline(userId):
-        return get(f"https://api.roblox.com/users/{userId}/onlinestatus/").json()["IsOnline"]
+    def is_online(user_id: int) -> bool:
+        """
+        Checks if User is currently online.
+        :param user_id: The User's ID to check for.
+        :return: True or False.
+        """
+        return get(f"https://api.roblox.com/users/{user_id}/onlinestatus/").json()["IsOnline"]
 
     @staticmethod
-    def GetOnlineStatus(userId):
-        return get(f"https://api.roblox.com/users/{userId}/onlinestatus/").json()
+    def get_online_status(user_id: int) -> dict:
+        """
+        Get's the User's online status.
+        :param user_id: The User's ID.
+        :return: A dictionary of the User's online status.
+        """
+        return get(f"https://api.roblox.com/users/{user_id}/onlinestatus/").json()
 
     @staticmethod
-    def CanManageAsset(userId, assetId):
-        return get("http://api.roblox.com/users/" + str(userId) + "/canmanage/" + str(assetId)).json() \
+    def can_manage_asset(user_id: int, asset_id: int) -> bool:
+        """
+        Checks if the User can manage a given asset.
+        :param user_id: The User's ID to check for.
+        :param asset_id: The Asset's ID to check for.
+        :return: True or False.
+        """
+        return get("http://api.roblox.com/users/" + str(user_id) + "/canmanage/" + str(asset_id)).json() \
             ["CanManage"]
