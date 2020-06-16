bb5322ebe55e0b0b55f9f76e8611af4840325a98
Jack Programs <52725440+jackprogramsjp@users.noreply.github.com>
2020-06-15 21:52:03 -0500

    Examples for understanding

--- examples/get_friends_groups.py
+++ examples/get_friends_groups.py
@@ -1,9 +1,9 @@
 from roblopy import Users, Friends, Groups
 
-MyUserId = Users.GetIdFromUsername("Coeptus")
+my_user_Id = Users.get_id_from_username("Coeptus")
 
-for user in Friends.GetFriends(MyUserId):
-    userId = user["Id"]
-    group = Groups.GetUsersGroups(userId)
+for user in Friends.get_friends(my_user_Id):
+    user_id = user["Id"]
+    group = Groups.get_users_groups(user_id)
 
     print(group)
