0ebfc318e7f4671d95b742cda4d22f00e60bd523
Jack Programs <52725440+jackprogramsjp@users.noreply.github.com>
2020-06-15 19:44:28 -0500

    Version 5.0

--- roblopy/assets.py
+++ roblopy/assets.py
@@ -1,54 +1,54 @@
 from .utils.request import get
 
+
 class Asset:
-    ProductType = None
-    AssetId = None
-    ProductId = None
-    Name = None
-    Description = None
-    AssetTypeId = None
-    Creator = None
-    CreatorId = None
-    CreatorType = None
-    Created = None
-    Updated = None
-    PriceInRobux = None
-    Sales = None
-    IsNew = None
-    IsForSale = None
-    IsPublicDomain = None
-    IsLimited = None
-    IsLimitedUnique = None
+    """
+    Represents a Roblox asset
+    """
+    def __init__(self, asset_id: int):
+        """
+        Constructs a new asset class.
+        :param asset_id: The Asset's ID.
+        """
+        response = get("https://api.roblox.com/Marketplace/ProductInfo?assetId=" + str(asset_id)).json()
 
-    def __init__(self, assetId):
-        response = get("https://api.roblox.com/Marketplace/ProductInfo?assetId=" + str(assetId)).json()
+        self.product_type = response["ProductType"]
+        self.asset_id = response["AssetId"]
+        self.product_id = response["ProductId"]
+        self.name = response["Name"]
+        self.description = response["Description"]
+        self.asset_type_id = response["AssetTypeId"]
+        self.creator = response["Creator"]["Name"]
+        self.creator_id = response["Creator"]["Id"]
+        self.creator_type = response["Creator"]["CreatorType"]
+        self.created = response["Created"]
+        self.updated = response["Updated"]
+        self.price_in_robux = response["PriceInRobux"]
+        self.sales = response["Sales"]
+        self.is_new = response["IsNew"]
+        self.is_for_sale = response["IsForSale"]
+        self.is_public_domain = response["IsPublicDomain"]
+        self.is_limited = response["IsLimited"]
+        self.is_limited_unique = response["IsLimitedUnique"]
 
-        self.ProductType = response["ProductType"]
-        self.AssetId = response["AssetId"]
-        self.ProductId = response["ProductId"]
-        self.Name = response["Name"]
-        self.Description = response["Description"]
-        self.AssetTypeId = response["AssetTypeId"]
-        self.Creator = response["Creator"]["Name"]
-        self.CreatorId = response["Creator"]["Id"]
-        self.CreatorType = response["Creator"]["CreatorType"]
-        self.Created = response["Created"]
-        self.Updated = response["Updated"]
-        self.PriceInRobux = response["PriceInRobux"]
-        self.Sales = response["Sales"]
-        self.IsNew = response["IsNew"]
-        self.IsForSale = response["IsForSale"]
-        self.IsPublicDomain = response["IsPublicDomain"]
-        self.IsLimited = response["IsLimited"]
-        self.IsLimitedUnique = response["IsLimitedUnique"]
 
 class Assets:
     @staticmethod
-    def GetAsset(assetId):
-        return get("https://api.roblox.com/Marketplace/ProductInfo?assetId=" + str(assetId)).json()
+    def get_asset(asset_id: int) -> dict:
+        """
+        Gets the asset's information.
+        :param asset_id: The Asset's ID.
+        :return: A dictionary of the asset's information.
+        """
+        return get("https://api.roblox.com/Marketplace/ProductInfo?assetId=" + str(asset_id)).json()
 
     @staticmethod
-    def UserHasAsset(userId, assetId):
+    def user_has_asset(user_id: int, asset_id: int) -> bool:
+        """
+        Checks if the User has the asset.
+        :param user_id: The User's ID to check for.
+        :param asset_id: The Asset's ID to check for.
+        :return: True or False.
+        """
         return get(
-            "https://api.roblox.com/Ownership/HasAsset?userId=" + str(userId) + "&assetId=" + str(assetId)).json()
-
+            "https://api.roblox.com/Ownership/HasAsset?userId=" + str(user_id) + "&assetId=" + str(asset_id)).json()
